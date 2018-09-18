4d71478e91297c7520c160dc7d4a4a5df1d819bb
Alberto G. Lagos <kronenr@gmail.com>
2018-09-18 12:15:12 +0200

    Añadimos componenentes Home y Demos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { LoggerService } from '../indra-core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [LoggerService]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Curso de Angular';
-
-  constructor(private out: LoggerService) {
-      out.debug('Comentario de debug');
-      out.info('Comentario de informacion');
-      out.warn('Comentario de warning');
-      out.error('Comentario de error');
-  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { IndraCoreModule } from '../indra-core';
+import { IndraCoreModule, ERROR_LEVEL, LoggerService } from '../indra-core';
 
 import { AppComponent } from './app.component';
 import { ComprasModule } from './compras/compras.module';
 import { FacturacionModule } from './facturacion/facturacion.module';
+import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+import { DemosComponent } from './demos/demos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, FormsModule,
-    IndraCoreModule,
-    ComprasModule, FacturacionModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        DemosComponent
+    ],
+    imports: [
+        BrowserModule, FormsModule,
+        IndraCoreModule,
+        ComprasModule, FacturacionModule,
+    ],
+    providers: [
+        LoggerService,
+        { provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
